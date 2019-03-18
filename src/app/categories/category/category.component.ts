fb7042b58b3b745840890bb302803e86600e2e1d
k1cho <aadnanhadzipasic@gmail.com>
2019-03-18 13:03:58 +0100

    get single category

--- src/app/categories/category/category.component.ts
+++ src/app/categories/category/category.component.ts
@@ -7,4 +7,25 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.scss']
 })
-export class CategoryComponent implements OnInit {}
+export class CategoryComponent implements OnInit {
+  category: any;
+  constructor(
+    private categoryService: CategoryService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.getCategory();
+  }
+
+  getCategory() {
+    this.categoryService
+      .getSingle(this.route.snapshot.paramMap.get('id'))
+      .subscribe(
+        data => {
+          this.category = data.category;
+        },
+        err => console.log(err)
+      );
+  }
+}
